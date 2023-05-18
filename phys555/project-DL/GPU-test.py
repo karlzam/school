767860b617c772e1fa97dc6c8fc0dde40c7bddd9
karlzam <karlee.zammit@gmail.com>
2023-05-18 16:03:22 -0700

    no message

--- phys555/project-DL/GPU-test.py
+++ phys555/project-DL/GPU-test.py
@@ -1,10 +1,10 @@
-import torch
 
-#print(torch.cuda.is_available())
-#a=torch.cuda.FloatTensor()
-
-#import keras
+import keras
 import tensorflow as tf
 tf.test.gpu_device_name()
-print(tf.config.list_physical_devices('GPU'))
+tf.config.list_physical_devices('GPU')
+
+import torch
+print(torch.cuda.is_available())
+a=torch.cuda.FloatTensor()
 print(torch.version.cuda)
